20e790185c503f0612ac76fc64a5d595bdb89625
Manasi-ghugretkar <75067968+Manasi-ghugretkar@users.noreply.github.com>
2021-06-19 10:18:07 +0530

    Faculty-Dashboard  page

---
+++ src/reusableComponent/errorToast.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+export default function Errortoast(msg) {
+    
+
+    return (toast.error(msg),{
+        position:"top-right",
+        autoClose:50000
+    });
+    
+}
